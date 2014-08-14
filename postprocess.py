farr/GalClusterFitting

commit 2e618dceab6dec3695a5fb3fca8bd6d6c5487567

Changed foreground plot to include all galaxies.

postprocess.py
@@ -108,9 +108,9 @@ def plot_membership(ftchain, logpost, path=None):
     for p in ftchain:
         log_pfores = np.logaddexp(log_pfores, logpost.log_pfore(p))
     log_pfores -= np.log(ftchain.shape[0])
-    
-    sel = (log_pfores > np.log(1e-8))
 
+    log_pfores = np.logaddexp(log_pfores, np.log(1e-12))
+    
     plt.figure()
     plt.scatter(logpost.ras[sel], logpost.decs[sel], c=np.exp(log_pfores[sel]), s=50, norm=mpl.colors.LogNorm(), alpha=0.5)
     plt.xlabel(r'$\alpha$ (deg)')

commit 12d3ee6bc933a2f7705688a7fb8593a8f0de9fb0

Fixed bug in postprocess.py introduced by 2e618dceab6d.

postprocess.py
@@ -112,7 +112,7 @@ def plot_membership(ftchain, logpost, path=None):
     log_pfores = np.logaddexp(log_pfores, np.log(1e-12))
     
     plt.figure()
-    plt.scatter(logpost.ras[sel], logpost.decs[sel], c=np.exp(log_pfores[sel]), s=50, norm=mpl.colors.LogNorm(), alpha=0.5)
+    plt.scatter(logpost.ras, logpost.decs, c=np.exp(log_pfores), s=50, norm=mpl.colors.LogNorm(), alpha=0.5)
     plt.xlabel(r'$\alpha$ (deg)')
     plt.ylabel(r'$\delta$ (deg)')
     plt.title(r'$p(\mathrm{member})$')